ca13d7f949697a6c85225882e27609f7b0538ef7
MattPua <matt.pua@utoronto.ca>
2013-09-28 23:51:36 -0400

    changed gravatar to circles

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
 
 	def gravatar_for(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
-    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
-    image_tag(gravatar_url, alt: user.name, class: "gravatar", style:"height:175px; width:175px;")
-  end
+	    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+	    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
+	    link_to (image_tag(gravatar_url, :alt => 'Avatar', class: "img-circle gravatar", style:"height:175px; width:175px;")) , user_path(user)
+  	end
 end

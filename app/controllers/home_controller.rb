a7ad6229f2170b8e8b9cf162f4af38a09415575e
MattPua <matt.pua@utoronto.ca>
2013-10-08 09:47:57 -0400

    Fixed main page for new users with no interviews

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -11,13 +11,15 @@ class HomeController < ApplicationController
   
   def main 
     @all_users = User.all
-    @company = Company.find(@user.next_interview.company_id)
-    @relevant_users=[]
-    @all_users.each do |user|
-      if user.companies.include?(@company) && user!=@user
-        @relevant_users.push(user)
-      # Create new array to hold users w/ similar industry or positions, not just same company
-      # Find way to shorten this action, will take long time. Maybe instead using SQL grab all the users with the same company?
+    if @user.has_interview?
+      @company = Company.find(@user.next_interview.company_id)
+      @relevant_users=[]
+      @all_users.each do |user|
+        if user.companies.include?(@company) && user!=@user
+          @relevant_users.push(user)
+        # Create new array to hold users w/ similar industry or positions, not just same company
+        # Find way to shorten this action, will take long time. Maybe instead using SQL grab all the users with the same company?
+        end
       end
     end
 

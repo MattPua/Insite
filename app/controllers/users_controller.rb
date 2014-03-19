1f92a9d906dbf00262ff7a26f63f938a7bdaa0d0
MattPua <matt.pua@utoronto.ca>
2014-03-19 01:05:36 -0400

    added tiny part to header dropdown

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,19 +9,9 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    if params[:user_search].present?
-      @users= User.search(params[:user_search])
-    elsif params[:company_search].present?
-      @companies=Company.search_by_company(params[:company_search])
-    elsif params[:interview_search].present?
-      @interviews=Interview.search_by_interview(params[:interview_search])
-    else
-      @users=User.all
-    end
-    @user= current_user
-   #@total=User.all
-    
 
+    @users=User.order("LOWER(name)").paginate(:page=>params[:page], :per_page =>12)
+  
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @users }
